8bed3c438d3d0b89e5ae1380906f05771e285e04
Botsi1 <98486983+Botsi1@users.noreply.github.com>
2022-06-27 12:06:03 -0300

    Delete breaking-bad directory

--- breaking-bad/src/components/Character.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default class Character extends React.Component {
-  render() {
-    return <div>Character</div>;
-  }
-}
--- breaking-bad/src/components/CharactersFav.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function CharactersFav() {
-  return <div>CharactersFav</div>;
-}

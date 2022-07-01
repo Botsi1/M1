df43e9b10c680568b66b4174dc4e91e565490083
Botsi1 <bmoreiro@gmail.com>
2022-07-01 13:18:40 -0300

    ACTZ2

--- FT-M3-master/03-WebServer/homework/Beatles/app.js
+++ FT-M3-master/03-WebServer/homework/Beatles/app.js
@@ -22,3 +22,53 @@ var beatles=[{
   profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
 }
 ]
+
+
+function readFile(filename){
+  return new Promise((resolve,reject)=>{
+    fs.readFile(filename,),'utf-8', (err, data)=>{
+      if(err) reject(err);
+      else resolve(data)
+    }  })
+}
+
+http.createServer(function(req,res){
+  if (req.url==="/"){
+    return readFile("./index.html").then((html)=>{
+      res.writeHead(200,{"Conten-Type": "application/json"});
+      return res.end(html);
+    })
+
+  }
+  if(req.url==="/api"){
+    res.writeHead(200,{"Content-Type": "application/json"});
+    return res.end(JSON.stringify(beatles))
+  }
+
+  // str = "pablito"   str.substring(0,2) pab
+  if(req.url.substring(0,5)==="/api/"){
+     // localhost:1337/A/B/C   req.url /A/B/John%20Lennon  ["A","B","C"]  "C"
+     const search = req.url.split+("/").pop()
+     const beatle = beatles.find((b)=>encodeURI(b.name)=== search)
+     if(beatle){
+      res.writeHead(200,{"Conten-Type" : "application/json"});
+      return res.end(JSON.stringify(beatle))
+     }
+  }
+  if(req.url[0]==="/" && req.url.length > 1){
+    console.log("entrando al aca")
+    const search= req.url.split("/").pop()
+    const beatle= beatles.find((b)=>encodeURI(b.name)=== search)
+    console.log("1----->", beatle)
+    return readFile("./beatle.html")
+          .then((html)=>{
+            console.log("2------>", beatle)
+            html= html.replace("{name}",beatle.name)
+            html= html.replace("{cumple}",beatle.birthdate)
+            hmtl = html.replace("{img}", beatle.profilePic)
+            res.writeHead(200,{"Conten-Type": "text/hmtl;charset=UFT-8"});
+            res.end(hmtl);
+          })
+
+  }
+})

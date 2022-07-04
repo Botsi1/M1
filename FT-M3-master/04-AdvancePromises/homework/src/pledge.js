61d203b7b734eafb4745e150cd47b307b6246fe6
Botsi1 <bmoreiro@gmail.com>
2022-07-04 12:42:06 -0300

    ACTZ4

--- FT-M3-master/04-AdvancePromises/homework/src/pledge.js
+++ FT-M3-master/04-AdvancePromises/homework/src/pledge.js
@@ -3,6 +3,44 @@
 Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
+function $Promise(executor){
+    if(typeof executor !== "function") throw new TypeError("Is not a function")
+    this._state = "pending"
+    this._handleGroups =[]
+
+    executor(this._internalreject.bind(this),this._internalreject.bind(this))
+}
+$Promise.prototype._internalresolve = function(value){
+  if(this_state === "pending"){
+    this_state = "fulfilled"
+    this._value = value
+  }
+
+}
+
+
+$Promise.prototype._internalreject = function(value){
+    if(this_state === "pending"){
+        this_state = "rejected"
+        this._value = value
+      }
+    
+}
+
+$Promise.prototype._internalreject = function(value){
+    if(this_state === "pending"){
+        this_state = "rejected"
+        this._value = value
+      }
+    
+}
+$Promise.prototype.then = function(successCb,errorCb){
+    if(typeof successCb !== "function") successCb = false
+    if(typeof errorCb!=="function") errorCb = false
+    this._handleGroups.push({successCb,errorCb})
+
+
+}
 
 
 

e4320dbe518d9a1e45ae7d20ce3c75e051bd8e19
Botsi1 <bmoreiro@gmail.com>
2022-07-05 15:41:52 -0300

    Listo

--- FT-M3-master/04-AdvancePromises/homework/src/pledge.js
+++ FT-M3-master/04-AdvancePromises/homework/src/pledge.js
@@ -14,6 +14,7 @@ $Promise.prototype._internalresolve = function(value){
   if(this_state === "pending"){
     this_state = "fulfilled"
     this._value = value
+    this.callHandlers()
   }
 
 }
@@ -23,26 +24,78 @@ $Promise.prototype._internalreject = function(value){
     if(this_state === "pending"){
         this_state = "rejected"
         this._value = value
+        this.callHandlers()
       }
     
 }
 
-$Promise.prototype._internalreject = function(value){
-    if(this_state === "pending"){
-        this_state = "rejected"
-        this._value = value
-      }
-    
-}
 $Promise.prototype.then = function(successCb,errorCb){
     if(typeof successCb !== "function") successCb = false
     if(typeof errorCb!=="function") errorCb = false
-    this._handleGroups.push({successCb,errorCb})
-
+    const downstreamPromise = new $Promise(function(){})
+    this._handleGroups.push({successCb,errorCb,downstreamPromise})
+    if(this_state !=="pending") this.callHandlers()
+    return downstreamPromise;
 
 }
 
+$Promise.prototype.callHandlers = function(){
+  while(this._handleGroups>0){
+    //[{sH1,eH1},{sH2,eH2}]
+    let current = this._handleGroups.shift(); 
+    if(this._state ==="fulfilled"){
+      if(!current.successCb){
+        current.downstreamPromise._internalresolve(this._value)
+      }else{
+        try {
+          const result = current.successCb(this._value)
+          // result = promise Z
+          if(result instanceof $Promise){
+            result.then(value => current.downstreamPromise._internalresolve(value),
+            err => current.downstreamPromise._internalreject(err))
+          }else{
+            current.downstreamPromise._internalresolve(result);
+          }
+          
+        } catch (e){
 
+          current.downstreamPromise._internalreject(e);
+          
+        }
+       
+      }
+      // current.successCb && current.successCb(this._value)
+    }
+
+    if(this._state ==="rejected"){
+      if(!current.errorCb){
+        current.downstreamPromise._internalreject(this._value)
+      }else{
+        try {
+          const result = current.errorCb(this._value)
+          if(result instanceof $Promise){
+            result.then(value => current.downstreamPromise._internalresolve(value),
+            err => current.downstreamPromise._internalreject(err))
+          }
+          else{
+            current.downstreamPromise._internalresolve(result)
+          }
+        } catch (error) {
+          current.downstreamPromise._internalreject(e);
+          
+        }
+      }
+
+      // current.errorCb && current.errorCb(this._value)
+    }
+
+  }
+}
+
+$Promise.prototype.catch = function(errorCb){
+  return this.then(null,errorCb);
+
+};
 
 module.exports = $Promise;
 /*-------------------------------------------------------
